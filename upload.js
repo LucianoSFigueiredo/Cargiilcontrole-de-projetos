aaa1f1832661aca121da2224ad588bcfa8aecfb5
LucianoSFigueiredo <130690893+LucianoSFigueiredo@users.noreply.github.com>
2023-05-02 21:40:19 -0300

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,32 +1,6 @@
-const { Storage } = require('@google-cloud/storage');
-const storage = new Storage({ keyFilename: './google-creds.json' });
-const bucketName = 'cargill-porto-franco';
-
-async function uploadFiles(files) {
-  const promises = files.map(async (file) => {
-    await storage.bucket(bucketName).upload(file.path, {
-      gzip: true,
-      metadata: {
-        cacheControl: 'public, max-age=31536000',
-      },
-    });
-    console.log(`${file.originalname} uploaded to ${bucketName}.`);
-  });
-  await Promise.all(promises);
-}
-
-const files = [
-  { originalname: 'nome-do-arquivo-1.extensao', path: '/caminho/do/arquivo/nome-do-arquivo-1.extensao' },
-  { originalname: 'nome-do-arquivo-2.extensao', path: '/caminho/do/arquivo/nome-do-arquivo-2.extensao' },
-  // adicione quantos arquivos quiser
-];
-
-
 const axios = require('axios');
 
-async function handleFileUpload() {
-  const fileInput = document.getElementById('file-input');
-  const files = fileInput.files;
+async function uploadFiles(files) {
   const formData = new FormData();
 
   for (let i = 0; i < files.length; i++) {
@@ -41,4 +15,12 @@ async function handleFileUpload() {
   }
 }
 
-document.getElementById('submit-button').addEventListener('click', handleFileUpload);
+const files = [
+  { originalname: 'nome-do-arquivo-1.extensao', path: '/caminho/do/arquivo/nome-do-arquivo-1.extensao' },
+  { originalname: 'nome-do-arquivo-2.extensao', path: '/caminho/do/arquivo/nome-do-arquivo-2.extensao' },
+  // adicione quantos arquivos quiser
+];
+
+document.getElementById('submit-button').addEventListener('click', () => {
+  uploadFiles(files);
+});
